2e82989a44c64e10e16c3dcb4d2442a1c7c12dbf
shabiir-anjum <mahrshabbir768@gmail.com>
2024-03-29 13:17:36 +0500

    update

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -17,40 +17,51 @@ const Projects = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
-          title="SOCIAL MEDIA CLONE"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          title="Dogy App | Gemini AI Heckathon"
+          des=" Contributed to a hackathon project, Dogy Walk Generator, innovating personalized dog walking routes for varied energy levels and safety concerns. Transformed daily walks for dog owners, ensuring both creativity and safety."
           src={projectOne}
+          link= 'https://dogy-app.vercel.app/'
+          git='https://github.com/Shabbir-Anjum'
         />
         <ProjectsCard
           title="E-commerce Website"
           des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
           src={projectTwo}
+          link= 'https://dogy-app.vercel.app/'
+          git='https://github.com/Shabbir-Anjum'
         />
         <ProjectsCard
           title="Chatting App"
           des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
           src={projectThree}
+          link= 'https://dogy-app.vercel.app/'
+          git='https://github.com/Shabbir-Anjum'
         />
         <ProjectsCard
           title="SOCIAL MEDIA CLONE"
           des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
           src={projectThree}
+          link= 'https://dogy-app.vercel.app/'
+          git='https://github.com/Shabbir-Anjum'
         />
         <ProjectsCard
           title="E-commerce Website"
           des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
           src={projectOne}
+          link= 'https://dogy-app.vercel.app/'
+          git='https://github.com/Shabbir-Anjum'
         />
         <ProjectsCard
           title="Chatting App"
           des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
           src={projectTwo}
+          link= 'https://dogy-app.vercel.app/'
+          git='https://github.com/Shabbir-Anjum'
         />
       </div>
     </section>
